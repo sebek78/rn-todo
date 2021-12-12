e711073188740ca518c085c5d52eb9030de28590
sebek78 <sebasporek@gmail.com>
2021-12-12 16:35:09 +0100

    Add React Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const TodosList = ({navigation}: any) => (
+  <Button
+    title="Add new todo"
+    onPress={() => navigation.navigate('TodoForm')}
+  />
+);
+
+const TodoForm = ({navigation}: any) => (
+  <Button
+    title="Back to todo list"
+    onPress={() => navigation.navigate('TodoList')}
+  />
+);
 
 const App = () => {
-  return <Text>Test</Text>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="TodoList">
+        <Stack.Screen
+          name="TodoList"
+          component={TodosList}
+          options={{title: 'To do:'}}
+        />
+        <Stack.Screen
+          name="TodoForm"
+          component={TodoForm}
+          options={{title: 'Add new Todo:'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
